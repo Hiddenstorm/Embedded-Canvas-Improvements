06f93711d3567b8ce0bf1cb85f5d64296814d4d5
HiddenStorm <mark_schuck@protonmail.com>
2024-09-08 01:11:05 +0200

    Initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,149 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class CanvasPlugin extends Plugin {
+    
+    // Will add function to zoom into bubbles on hover
+    // image support
+    // include paths
 
-interface MyPluginSettings {
-	mySetting: string;
+    async onload() {
+
+        console.log('Loading canvas plugin');
+        
+        // reading mode or rendering for html / pdf
+        // goes through each markdown block to check if theres an embed present
+        this.registerMarkdownPostProcessor((element, context) => {
+
+            const elements = element.querySelectorAll(".internal-embed");
+
+            elements.forEach(element => {
+                const linkSource = element.getAttribute('src');
+                    if (linkSource && linkSource.endsWith('.canvas')) {
+                        console.log("Canvas embedded:", linkSource);
+                        // add text
+                    }
+            });
+
+        })
+
+        const observer = new MutationObserver((mutationsList) => {
+            mutationsList.forEach((mutation) => {
+                
+                // whenever new html node is created/ modified
+                if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+                    
+                    mutation.addedNodes.forEach((node) => {
+                        
+                        if (node instanceof HTMLElement) {
+                            
+                            // Fires whenever document is completely rerendered such as when the page is made active
+                            const embeds = node.querySelectorAll(".internal-embed");
+                            embeds.forEach((embed) => {
+                                
+                                manipulateEmbed(embed);
+                            
+                            });
+                            
+                            // Fires when actively added html block is an embedded canvas (basically newly made ones)
+                            if (node.classList.contains("internal-embed")) {
+                                const linkSource = node.getAttribute('src');
+                                if (linkSource && linkSource.endsWith('.canvas')) {
+                                    console.log("vhbaskhgasvdf")
+                                    manipulateEmbed(node)
+
+                                }
+                            }
+                        } 
+                        // ignores if the modified node is just text
+                        else if (node.nodeType === Node.TEXT_NODE) {
+                            
+                            return;
+                        
+                        }
+                    });
+                }
+            });
+        });
+        
+        const targetNode = document.querySelector('.workspace-split') || document.body;
+        observer.observe(targetNode, { childList: true, subtree: true });
+        
+    }
+
+    onunload() {
+        console.log('Unloading canvas plugin');
+    }
+
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+function manipulateEmbed(embed: Element){
+    
+    const canvasname = embed.getAttribute('src');
+                                
+    if (canvasname && canvasname.endsWith('.canvas')) {
+        
+        // !Could be a problem in the future if theres different files with the same name in different paths
+        // Looping through files until file name fits because theres no
+        // function to get the full file path from the html element
+        // for ex. <div class="internal-embed canvas-embed inline-embed is-loaded" tabindex="-1" src="TestCanvas.canvas" alt="TestCanvas" contenteditable="false">
+        // src only showing me file name
+        const files = this.app.vault.getFiles();
+        let canvas: TFile | undefined;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        for (let index = 0; index < files.length; index++) {
+            
+            const file = files[index];
+            
+            if(file.name==canvasname){
+                
+                canvas=file;
+                break;
+            
+            }
+            
+        }
+        
+        if(canvas){
 
-	async onload() {
-		await this.loadSettings();
+            this.app.vault.read(canvas).then((content: any)=>{
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+                const canvasjson = JSON.parse(content);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+                const rects = embed.querySelector(".canvas-minimap")?.querySelectorAll("rect")
+                if(rects){
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                    for (let i = 0; i < rects.length; i++) {
+                        const rect = rects[i];
+                        
+                        const minimap : SVGElement = (rect.parentElement as SVGElement & HTMLElement)
+                        const group = minimap.createSvg("g")
+                        
+                        group.appendChild(rect)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+                        const text = group.createSvg("text")
+                        text.setText(canvasjson["nodes"][i].text)
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+                        text.setAttribute("x", (rect.x.baseVal.value + rect.width.baseVal.value/2).toString())
+                        // Text weirdly offset so +3 should fix for now
+                        text.setAttribute("y", ((rect.y.baseVal.value + rect.height.baseVal.value/2) + 3).toString())
+                        text.setAttribute("text-anchor", "middle")
+                        text.setAttribute("dominant-baseline", "middle")
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+                        // css variables dont work on svg elements such as <text> so render it with the css markdown-preview-view class
+                        // to get the theme variables and add them as svg ones
+                        text.addClass("markdown-preview-view")
+                        text.setAttribute("fill", getComputedStyle(text).getPropertyValue("color"))
+                        text.setAttribute("font-family", getComputedStyle(text).getPropertyValue("font-family"))
+                        text.setAttribute("font-size", getComputedStyle(text).getPropertyValue("font-size"))
+                        
+                    }
+                
+                }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+            });
 
-	onunload() {
+        }
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    }
+    
 }
